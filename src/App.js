21f269934aaecd7b611ed232e3c4110b4c15c9a9
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-07-22 23:02:26 -0400

    feat: the file App.js import all component to complete the aplication, and index.css has all variables global

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './modules/presentation/presentation';
+import Main from './modules/main/main';
+import Footer from './modules/footer/footer';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+
+      
+    </div>
+  );
+}
+
+export default App;
