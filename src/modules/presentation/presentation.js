221af3b7049354bdbb1ad0a10d41f56c59cf8a91
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-07-22 22:55:42 -0400

    feat: create and styling the first component this component is presentation of title of landing page

---
+++ src/modules/presentation/presentation.js
@@ -0,0 +1,18 @@
+import './presentation.css';
+
+
+function Header() {
+
+    return(
+
+        <div className='header'>
+            <h1>Workshop</h1>
+            <h2>Fogueira dos Boletos</h2>
+        </div>
+    )
+
+
+}
+
+
+export default Header;
